521b7110792f511af3cc72c3d0ba1d175b3a9064
j19015 <j19015@sangi.jp>
2022-07-11 13:59:40 +0900

    Add Admin to model & End_user column changed password

--- db/migrate/20220710105544_devise_create_admins.rb
+++ db/migrate/20220710105544_devise_create_admins.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-class DeviseCreateEndUsers < ActiveRecord::Migration[6.1]
+class DeviseCreateAdmins < ActiveRecord::Migration[6.1]
   def change
-    create_table :end_users do |t|
+    create_table :admins do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
-      t.string :password, null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
@@ -31,20 +31,14 @@ class DeviseCreateEndUsers < ActiveRecord::Migration[6.1]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-      t.string :last_name
-      t.string :first_name
-      t.string :last_name_kana
-      t.string :first_name_kana
-      t.string :post_code
-      t.string :address
-      t.string :phone_number
-      t.boolean :is_deleted,default: false
+
+
       t.timestamps null: false
     end
 
-    add_index :end_users, :email,                unique: true
-    add_index :end_users, :reset_password_token, unique: true
-    # add_index :end_users, :confirmation_token,   unique: true
-    # add_index :end_users, :unlock_token,         unique: true
+    add_index :admins, :email,                unique: true
+    add_index :admins, :reset_password_token, unique: true
+    # add_index :admins, :confirmation_token,   unique: true
+    # add_index :admins, :unlock_token,         unique: true
   end
 end
