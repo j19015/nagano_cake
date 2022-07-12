93b14ceaeef1a7298fad7bfd155eedd92a2a15f4
j19015 <j19015@sangi.jp>
2022-07-12 16:37:24 +0900

    admin end_user views create && update End_user password->encrypted_password

--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2022_07_11_065451) do
 
   create_table "end_users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "password", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
