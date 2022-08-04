738c6939fdbf4564cb2c3533d55666264866c8dd
j19015 <j19015@sangi.jp>
2022-08-05 03:28:50 +0900

    delicary complete ver1.0

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,8 +1,36 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
-    order_detail=OrderDetail.find(params[:id])
-    order_detail.update(order_status_params)
-    redirect_to admin_order_path(order_detail.order.id)
+    @order_detail=OrderDetail.find(params[:id])
+    @order_detail.update(order_status_params)
+
+    #②製作ステータスが一つでも 製作中 → 注文ステータスを 製作中
+    #③製作ステータスが全て 製作完了 → 注文ステータスを 発送準備中
+
+    #order1が制作ステータスが一つでも製作中を判断するための変数
+    order1=false
+    #order2が制作ステータスが全部制作完了かどうかを判断するための変数
+    order2=0
+    #@orderに注文詳細の親の注文情報を格納
+    @order=@order_detail.order
+    #注文情報に紐づいた注文詳細の中をトレースして精査
+    @order.order_details.each do |order_detail|
+      if OrderDetail.order_statuses[order_detail.order_status]==2
+        #製作中なら
+        order1=true
+      elsif OrderDetail.order_statuses[order_detail.order_status]==3
+        #制作完了なら
+        order2+=1
+      end
+    end
+
+    if order2==@order.order_details.count
+      #全ての商品が制作完了している場合
+      @order.update(order_status:3)    
+    elsif order1==true
+      #一つでも製作中なら
+      @order.update(order_status:2)
+    end
+    redirect_to admin_order_path(@order_detail.order.id)
   end
   private
   def order_status_params
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -10,6 +10,12 @@ class Admin::OrdersController < ApplicationController
   def update
     @order=Order.find(params[:id])
     @order.update(order_status_params)
+     #①注文ステータスが 入金確認 → 全ての製作ステータスを 製作待ち
+     if Order.order_statuses[@order.order_status]==1
+      @order.order_details.each do |order_detail|
+        order_detail.update(order_status:1)
+      end
+    end
     redirect_to admin_order_path(@order.id)
   end
   private
